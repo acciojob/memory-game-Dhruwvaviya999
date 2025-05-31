14a50eed584eaa6c61c72112c52327e37399fb69
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-05-31 10:11:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 
 import React from "react";
 import './../styles/App.css';
+import WelcomePage from "../pages/WelcomePage";
+import EasyModePage from "../pages/EasyModePage";
+import HardModePage from "../pages/HardModePage";
+import NormalModePage from "../pages/NormalModePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const App = () => {
+function App() {
+  const [levelChoice, setLevelChoice] = useState("");
+
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<WelcomePage setLevelChoice={setLevelChoice} levelChoice={levelChoice} />} />
+            <Route path="/hard-mode" element={<HardModePage/>} />
+            <Route path="/normal-mode" element={<NormalModePage/>} />
+            <Route path="/easy-mode" element={<EasyModePage/>} />
+          </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
